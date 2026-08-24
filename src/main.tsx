adc4b825356e933d394e12a707fb3f7333d3dcf0
dom-elves <79201054+dom-elves@users.noreply.github.com>
2026-08-24 14:23:58 +0100

    move everything out of main.tsx as apparently that's wrong, remove boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Product from './Product.tsx'
-
-// make request
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 className="title">Office Essentials</h1>
-    <div className="filter-container">
-      <button className="filter">Sort By Price</button>
-      <button className="filter">Sort By Review</button>
-      <button className="filter">Sort By Name</button>
-      <button className="filter">Sort By Saving</button>
-    </div>
-    <Product></Product>
+    <App></App>
   </StrictMode>,
 )
