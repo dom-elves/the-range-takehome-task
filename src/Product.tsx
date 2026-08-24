8b4f34c510e42ec68ca0855e58243a3b43040add
dom-elves <79201054+dom-elves@users.noreply.github.com>
2026-08-24 21:21:07 +0100

    format currencies for display

--- src/Product.tsx
+++ src/Product.tsx
@@ -2,17 +2,32 @@ import './Product.css'
 
 function Product({product}) {
 
-    console.log('test');
+    /**
+     * format currency values for display
+     */
+    const formatCurrency = (amount: number) => (amount / 100).toFixed(2);
+
+    const formattedPrice = formatCurrency(product.price);
+    const formattedWasPrice = formatCurrency(product.was_price);
+
     return (
         <div className="grid-item product-card">
             <img src={`/product-images/${product.img}.jpg`} />
             <div className="product-info">
                 <p className="product-name">{product.name}</p>
-                <p className="product-price">£{product.price}</p>
-                <p className="product-was-price">
-                    Was <span>£{product.was_price}</span>
+                <p className="product-price">£{formattedPrice}</p>
+                <p 
+                    className="product-was-price" 
+                    style={{ visibility: formattedWasPrice > 0 ? 'visible' : 'hidden' }}
+                >
+                    Was <span>£{formattedWasPrice}</span>
                 </p>
-                <p className="product-reviews">{product.reviews}% Review Score</p>
+                <p 
+                    className="product-reviews" 
+                    style={{ visibility: product.reviews ? 'visible' : 'hidden' }}
+                >
+                    {product.reviews}% Review Score
+                </p>
                 <button className="basket-button">Add To Basket</button>
             </div>
         </div>
