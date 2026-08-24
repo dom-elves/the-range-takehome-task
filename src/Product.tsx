47f6c4821031e084f0de8f5650ae3945f6929ccc
dom-elves <79201054+dom-elves@users.noreply.github.com>
2026-08-24 16:30:48 +0100

    basic product component, currently unfinished

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,6 +1,19 @@
+import './Product.css'
+
 function Product({product}) {
     return (
-        <p>{product.name}</p>
+        <div className="grid-item product-card">
+            <img src={`/product-images/${product.img}.jpg`} />
+            <div className="product-info">
+                <p className="product-name">{product.name}</p>
+                <p className="product-price">£{product.price}</p>
+                <p className="product-was-price">
+                    Was <span>£{product.was_price}</span>
+                </p>
+                <p className="product-reviews">{product.reviews}% Review Score</p>
+                <button className="basket-button">Add To Basket</button>
+            </div>
+        </div>
     )
 }
 
